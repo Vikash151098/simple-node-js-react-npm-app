8930c55e74ce2cb6075c0ddb3ffa889114e5cdd1
Vikash Kumar <vkumar0961@gmail.com>
2024-01-07 13:35:53 +0530

    edited deploy.sh and commented deliver.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,5 @@
 # deploying into nginx
-cp ./build/* /usr/share/nginx/html/
+sudo cp -r ./build/* /usr/share/nginx/html/
 
 # docker stop web
 # docker run -it --rm -d -p 8081:80 --name web -v ~/site-content:/usr/share/nginx/html nginx
