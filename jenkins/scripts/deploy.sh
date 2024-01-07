0764ad35568eab678cac8e3eec094526dd6b4c6d
Vikash Kumar <vkumar0961@gmail.com>
2024-01-07 13:39:59 +0530

    added file permission in deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,6 @@
 # deploying into nginx
+set -x
 sudo cp -r ./build/* /usr/share/nginx/html/
-
+set +x
 # docker stop web
 # docker run -it --rm -d -p 8081:80 --name web -v ~/site-content:/usr/share/nginx/html nginx
